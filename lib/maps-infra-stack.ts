1049d7b1239fabd5d0b0e2457d11c1c4f6bbee35
Sauli Anto <sauli.anto@gmail.com>
2022-04-04 01:15:54 +0300

    Disable compress behavior and add CORS allow all

--- lib/maps-infra-stack.ts
+++ lib/maps-infra-stack.ts
@@ -3,7 +3,11 @@ import { Construct } from "constructs";
 import { aws_s3 as s3 } from "aws-cdk-lib";
 import { aws_cloudfront as cloudfront } from "aws-cdk-lib";
 import { aws_cloudfront_origins as origins } from "aws-cdk-lib";
-import { PriceClass } from "aws-cdk-lib/aws-cloudfront";
+import {
+  CachePolicy,
+  PriceClass,
+  ResponseHeadersPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
 import { BlockPublicAccess } from "aws-cdk-lib/aws-s3";
 
 export class MapsInfraStack extends Stack {
@@ -15,7 +19,17 @@ export class MapsInfraStack extends Stack {
     });
 
     new cloudfront.Distribution(this, "mapsTilesDistribution", {
-      defaultBehavior: { origin: new origins.S3Origin(mapsTilesBucket) },
+      defaultBehavior: {
+        origin: new origins.S3Origin(mapsTilesBucket),
+        // Files in the Bucket are already compressed
+        // Metadata:
+        // - Content-Type: application/x-protobuf
+        // - Content-Encoding: gzip
+        // - Cache-Control: public, max-age=31536000
+        compress: false,
+        cachePolicy: CachePolicy.CACHING_OPTIMIZED_FOR_UNCOMPRESSED_OBJECTS,
+        responseHeadersPolicy: ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS,
+      },
       priceClass: PriceClass.PRICE_CLASS_100,
     });
   }
